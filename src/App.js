bb5f9be1cedc810d0e3b6b37463d45325b47430d
Sharoz Ali <sharoz.ali@simplifipay.com>
2023-11-03 17:52:59 +0500

    create layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/Dashboard';
+import DashboardLayout from './layout/dashboardLayout';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <DashboardLayout>
+        <Dashboard />
+      </DashboardLayout>
     </div>
   );
 }
