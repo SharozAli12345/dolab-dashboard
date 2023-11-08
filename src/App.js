e6b7daab4aaa20bffbc544b87656d0ccdf6db9a2
Sharoz Ali <sharoz.ali@simplifipay.com>
2023-11-08 17:14:50 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import Dashboard from './pages/Dashboard';
+// import Dashboard from './pages/Dashboard';
 import DashboardLayout from './layout/dashboardLayout';
 import Invoice from './pages/Invoice';
-import Balance from './pages/Balance';
+// import Balance from './pages/Balance';
 import Login from './pages/Authentication/Login';
 import Register from './pages/Authentication/Register';
-import AddBeneficiary from './pages/Dashboard/Benificiary/addBenificiary';
-import Beneficiaries from './pages/Dashboard/Benificiary/beneficiaries';
-import TransactionDetails from './pages/Transactions/transactionDetails';
-import TransactionsHistory from './pages/Transactions/transactionHistory';
-import BusinessPlans from './pages/Plans/businessPlans';
-import Profile from './pages/Profile';
+// import AddBeneficiary from './pages/Dashboard/Benificiary/addBenificiary';
+// import Beneficiaries from './pages/Dashboard/Benificiary/beneficiaries';
+// import TransactionDetails from './pages/Transactions/transactionDetails';
+// import TransactionsHistory from './pages/Transactions/transactionHistory';
+// import BusinessPlans from './pages/Plans/businessPlans';
+// import Profile from './pages/Profile';
 import AccountConfirmation from './pages/Account/accountConfirmation';
-
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
-  
+
   return (
-   <DashboardLayout>
-       <AccountConfirmation /> 
-   </DashboardLayout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<DashboardLayout>
+          <AccountConfirmation />
+        </DashboardLayout>} />
+        <Route path="/invoice" element={<DashboardLayout>
+          <Invoice />
+        </DashboardLayout>} />
+        <Route path="/login" element={
+          <Login />
+        } />
+        <Route path="/register" element={
+          <Register />
+        } />
+      </Routes>
+ƒ    </BrowserRouter>
+
   );
 }
 
